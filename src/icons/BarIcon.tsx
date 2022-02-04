b04b0f67f4b8a9b35f82b13f09761cafcc174690
Yen Truong <ytruong@yext.com>
2022-02-04 10:13:51 -0500

    add icon files

---
+++ src/icons/BarIcon.tsx
@@ -0,0 +1,7 @@
+export default function BarIcon(): JSX.Element {
+  return (
+    <svg width="1" height="24" viewBox="0 0 1 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="1" height="24" rx="0.5" fill="#E1E5E8" />
+    </svg>
+  );
+}
