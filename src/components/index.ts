9e6c6d63cb54a3e4a41e1cb6bdb1ae481ac6d69f
Yen Truong <ytruong@yext.com>
2022-02-07 15:57:45 -0500

    add spellcheck

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,8 @@ export type {
 } from './SearchBar';
 export { default as EntityPreviews } from './EntityPreviews';
 export { default as DropdownItem } from './Dropdown/DropdownItem';
+export { default as SpellCheck } from './SpellCheck';
+export type {
+  SpellCheckCssClasses,
+  SpellCheckProps
+} from './SpellCheck';
