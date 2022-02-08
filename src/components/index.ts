ff5e21cf1977fea507fe95f2038b94b2f6058e0d
Oliver Shi <shishishi123@gmail.com>
2022-02-08 11:11:47 -0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export type {
   SearchBarCssClasses,
   SearchBarProps
 } from './SearchBar';
-
 export { default as EntityPreviews } from './EntityPreviews';
 export { default as DropdownItem } from './Dropdown/DropdownItem';
 
