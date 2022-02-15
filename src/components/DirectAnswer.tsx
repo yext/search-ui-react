e3d23508d2eb9d482beb3aa26ed1f6feee6f5992
Yen Truong <ytruong@yext.com>
2022-02-15 12:38:32 -0500

    add feedback and cta analytics to direct answers

--- src/components/DirectAnswer.tsx
+++ src/components/DirectAnswer.tsx
@@ -1,8 +1,10 @@
 import { useAnswersState, DirectAnswerType, DirectAnswer as DirectAnswerModel } from '@yext/answers-headless-react';
 import renderHighlightedValue from './utils/renderHighlightedValue';
 import classNames from 'classnames';
-import { ReactNode } from 'react';
+import { ReactNode, useState, useLayoutEffect } from 'react';
 import { CompositionMethod, useComposedCssClasses } from '../hooks/useComposedCssClasses';
+import { useAnalytics } from '../hooks';
+import ThumbIcon from '../icons/ThumbIcon';
 
 export interface DirectAnswerProps {
   customCssClasses?: DirectAnswerCssClasses,
@@ -19,11 +21,18 @@ export interface DirectAnswerCssClasses {
   featuredSnippetDescription?: string,
   viewDetailsLink?: string,
   viewDetailsLinkContainer?: string,
-  highlighted?: string
+  highlighted?: string,
+  answerContainer?: string,
+  feedbackButtonsContainer?: string,
+  feedbackButton?: string,
+  thumbsUpIcon?: string,
+  thumbsDownIcon?: string
 }
 
+type FeedbackType = 'THUMBS_UP' | 'THUMBS_DOWN';
+
 const builtInCssClasses: DirectAnswerCssClasses = {
-  container: 'p-4 border rounded-lg shadow-sm',
+  container: '',
   container___loading: 'opacity-50',
   fieldValueTitle: 'mb-4 text-gray-500',
   featuredSnippetTitle: 'mb-4 font-bold text-xl text-gray-800',
@@ -32,14 +41,26 @@ const builtInCssClasses: DirectAnswerCssClasses = {
   featuredSnippetDescription: '',
   viewDetailsLink: 'text-blue-600',
   viewDetailsLinkContainer: 'pt-4 text-gray-500',
-  highlighted: 'bg-blue-100'
+  highlighted: 'bg-blue-100',
+  answerContainer: 'p-4 border rounded-lg shadow-sm',
+  feedbackButtonsContainer: 'flex justify-end mt-2 text-sm text-gray-400 font-medium',
+  thumbsUpIcon: 'ml-3 w-5',
+  thumbsDownIcon: 'w-5 ml-1 transform rotate-180',
 };
 
 export default function DirectAnswer(props: DirectAnswerProps): JSX.Element | null {
+  const analytics = useAnalytics();
   const directAnswerResult = useAnswersState(state => state.directAnswer.result);
   const isLoading = useAnswersState(state => state.searchStatus.isLoading || false);
   const composedCssClasses = useComposedCssClasses(
     builtInCssClasses, props.customCssClasses, props.cssCompositionMethod);
+  const verticalKey = useAnswersState(state => state.vertical.verticalKey);
+  const queryId = useAnswersState(state => state.query.queryId);
+  const [isFeedbackProvided, updateFeedbackStatus] = useState(false);
+  useLayoutEffect(() => {
+    updateFeedbackStatus(false);
+  }, [directAnswerResult, updateFeedbackStatus]);
+
   if (!directAnswerResult) {
     return null;
   }
@@ -52,18 +73,60 @@ export default function DirectAnswer(props: DirectAnswerProps): JSX.Element | nu
     : directAnswerResult.value;
   const link = directAnswerResult.relatedResult.link;
 
+  const reportCtaEvent = () => {
+    const entityId = directAnswerResult.relatedResult.id;
+    const fieldName = directAnswerResult.relatedResult.name;
+    if(!queryId) {
+      console.error('Unable to report a CTA event. Missing field: queryId.');
+      return;
+    }
+    if(!entityId) {
+      console.error('Unable to report a CTA event. Missing field: entityId.');
+      return;
+    }
+    analytics?.report({
+      type: 'CTA_CLICK',
+      entityId,
+      searcher: verticalKey ? 'VERTICAL' : 'UNIVERSAL',
+      queryId,
+      verticalKey: verticalKey || '',
+      directAnswer: true,
+      url: link,
+      fieldName
+    });
+  };
+
+  const reportFeedbackEvent = (feedbackType: FeedbackType) => {
+    if(!queryId) {
+      console.error('Unable to report a direct answer feedback event. Missing field: queryId.');
+      return;
+    }
+    const entityId = directAnswerResult.relatedResult.id;
+    analytics?.report({
+      type: feedbackType,
+      entityId,
+      searcher: verticalKey ? 'VERTICAL' : 'UNIVERSAL',
+      queryId: queryId,
+      verticalKey: verticalKey || '',
+      directAnswer: true,
+    });
+  };
+
   function getLinkText(directAnswerResult: DirectAnswerModel) {
     const isSnippet = directAnswerResult.type === DirectAnswerType.FeaturedSnippet;
     const name = directAnswerResult.relatedResult.name;
+    const onclick = () => {
+      analytics && reportCtaEvent();
+    };
 
     return (<>
       {isSnippet && name && <div className={cssClasses.viewDetailsLinkContainer}>
-        Read more about <a className={cssClasses.viewDetailsLink} href={link}>
+        Read more about <a className={cssClasses.viewDetailsLink} href={link} onClick={onclick}>
           {directAnswerResult.relatedResult.name}
         </a>
       </div>}
       {!isSnippet && link && <div className={cssClasses.viewDetailsLinkContainer}>
-        <a href={link} className={cssClasses.viewDetailsLink}>View Details</a>
+        <a href={link} className={cssClasses.viewDetailsLink} onClick={onclick}>View Details</a>
       </div>}
     </>);
   }
@@ -72,13 +135,38 @@ export default function DirectAnswer(props: DirectAnswerProps): JSX.Element | nu
     [cssClasses.container___loading ?? '']: isLoading
   });
 
+  const onClickFeedbackButton = (feedbackType: FeedbackType) => {
+    updateFeedbackStatus(true);
+    analytics && reportFeedbackEvent(feedbackType);
+  };
+
   return (
     <div className={containerCssClasses}>
-      {title &&
-        <div className={cssClasses.title}>{title}</div>}
-      <div className={cssClasses.content}>
-        <div className={cssClasses.description}>{description}</div>
-        {link && getLinkText(directAnswerResult)}
+      <div className={cssClasses.answerContainer}>
+        {title &&
+          <div className={cssClasses.title}>{title}</div>}
+        <div className={cssClasses.content}>
+          <div className={cssClasses.description}>{description}</div>
+          {link && getLinkText(directAnswerResult)}
+        </div>
+      </div>
+      <div className={cssClasses.feedbackButtonsContainer}>
+        {isFeedbackProvided
+          ? 'Thank you for your feedback!'
+          : <>Feedback
+            <button
+              className={cssClasses.thumbsUpIcon}
+              onClick={() => onClickFeedbackButton('THUMBS_UP')}
+            >
+              <ThumbIcon/>
+            </button>
+            <button
+              className={cssClasses.thumbsDownIcon}
+              onClick={() => onClickFeedbackButton('THUMBS_DOWN')}
+            >
+              <ThumbIcon/>
+            </button></>
+        }
       </div>
     </div>
   );
