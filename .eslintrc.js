6b27f66c802431f0fe89c79a99ebafd86fdcba37
Connor Anderson <canderson@yext.com>
2022-02-22 12:38:23 -0500

    warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,27 +12,27 @@ module.exports = {
     'eslint-plugin-import'
   ],
   rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'no-trailing-spaces' : ['error'],
-    'no-multi-spaces' : ['error'],
-    quotes: ['error', 'single'],
-    'object-curly-spacing': ['error', 'always'],
-    'space-before-function-paren': ['error', {
+    indent: ['warn', 2, { SwitchCase: 1 }],
+    'no-trailing-spaces' : ['warn'],
+    'no-multi-spaces' : ['warn'],
+    quotes: ['warn', 'single'],
+    'object-curly-spacing': ['warn', 'always'],
+    'space-before-function-paren': ['warn', {
       named: 'never',
       anonymous: 'never'
     }],
-    'keyword-spacing': ['error'],
-    'quote-props': ['error', 'as-needed'],
-    'max-len': ['error', {
+    'keyword-spacing': ['warn'],
+    'quote-props': ['warn', 'as-needed'],
+    'max-len': ['warn', {
       code: 110,
       ignorePattern: '(^import\\s.+\\sfrom\\s.+;$|<path.+>$)',
       ignoreStrings: true
     }],
-    '@typescript-eslint/semi': ['error'],
-    '@typescript-eslint/type-annotation-spacing': ['error'],
+    '@typescript-eslint/semi': ['warn'],
+    '@typescript-eslint/type-annotation-spacing': ['warn'],
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    'import/no-default-export': ['error']
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'import/no-default-export': ['warn']
   },
   ignorePatterns: ['lib', 'tests/setup/responses']
 };
