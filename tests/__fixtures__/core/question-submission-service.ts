8bf5875835831fe7fec65c8c8507c19fe151ba26
Tom Meyer <tmeyer@yext.com>
2022-03-04 13:44:21 -0500

    Add test fixtures and functions for mocking Headless. (#52)
    This PR adds a number of test fixtures and mock generators. The purpose of all these is to provide a mocked, controlled instance of Headless. This will allow us to write better unit tests as well as visual regression tests.
    
    J=SLAP-1928
    TEST=manual
    
    Made sure I could generate a fully mocked Headless instance, from which I generated Percy Snapshots (using Storybook).

---
+++ tests/__fixtures__/core/question-submission-service.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { QuestionSubmissionRequest, QuestionSubmissionResponse, QuestionSubmissionService } from '@yext/answers-core';
+
+/* A sample Question Submission response. */
+const mockedResponse: QuestionSubmissionResponse = {
+  uuid: '123'
+};
+
+/**
+ * Generates a mocked QuestionSubmissionService using the provided response.
+ *
+ * @param submissionResponse - The QuestionSubmissionResponse to return for all requests. If one is not
+ * provided, a default will be used.
+ */
+export function generateMockedQuestionSubmissionService(
+  submissionResponse?: QuestionSubmissionResponse): QuestionSubmissionService
+{
+  return {
+    submitQuestion: (request: QuestionSubmissionRequest) => {
+      const response = submissionResponse ? submissionResponse : mockedResponse;
+      return Promise.resolve(response);
+    }
+  };
+}
+
+export const MockedQuestionSubmissionService: QuestionSubmissionService =
+  generateMockedQuestionSubmissionService();
