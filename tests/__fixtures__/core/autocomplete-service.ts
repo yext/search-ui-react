adba8813c414315d35b6c86a8dc519febe52a340
Tom Meyer <tmeyer@yext.com>
2022-03-04 13:44:21 -0500

    Add test fixtures and functions for mocking Headless. (#52)
    This PR adds a number of test fixtures and mock generators. The purpose of all these is to provide a mocked, controlled instance of Headless. This will allow us to write better unit tests as well as visual regression tests.
    
    J=SLAP-1928
    TEST=manual
    
    Made sure I could generate a fully mocked Headless instance, from which I generated Percy Snapshots (using Storybook).

---
+++ tests/__fixtures__/core/autocomplete-service.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AutocompleteResponse, AutocompleteService, FilterSearchRequest, FilterSearchResponse, UniversalAutocompleteRequest, VerticalAutocompleteRequest } from '@yext/answers-core';
+
+/* A sample Autocomplete response. */
+const mockedResponse: AutocompleteResponse = {
+  results: [],
+  inputIntents: [],
+  queryId: 'query-id',
+  uuid: '123'
+};
+
+/* A sample Filter search response. */
+const mockedFilterSearchResponse: FilterSearchResponse = {
+  uuid: '123',
+  queryId: 'query-id',
+  sections: [],
+  businessId: '1234'
+};
+
+/**
+ * Generates a mocked AutocompleteService using the provided responses.
+ *
+ * @param response - The response to supply for all Query Suggestion requests. If one is
+ * not provided, a default will be used.
+ * @param filterSearchResponse - The response to supply for all Filter searches. If one is
+ * not provided, a default will be used.
+ */
+export function generateMockedAutocompleteService(
+  autocompleteResponse?: AutocompleteResponse,
+  filterSearchResponse?: FilterSearchResponse): AutocompleteService
+{
+  return {
+    universalAutocomplete:
+      (request: UniversalAutocompleteRequest) => {
+        const response = autocompleteResponse ? autocompleteResponse : mockedResponse;
+        return Promise.resolve(response);
+      },
+    verticalAutocomplete:
+      (request: VerticalAutocompleteRequest) => {
+        const response = autocompleteResponse ? autocompleteResponse : mockedResponse;
+        return Promise.resolve(response);
+      },
+    filterSearch:
+      (request: FilterSearchRequest) => {
+        const response = filterSearchResponse ? filterSearchResponse : mockedFilterSearchResponse;
+        return Promise.resolve(response);
+      }
+  };
+}
+
+export const MockedAutocompleteService: AutocompleteService = generateMockedAutocompleteService();
---
+++ tests/__fixtures__/core/search-service.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { SearchService, UniversalSearchRequest, UniversalSearchResponse, VerticalSearchRequest, VerticalSearchResponse } from '@yext/answers-core';
+
+/* A sample Universal Search response. */
+const mockedUniversalResponse: UniversalSearchResponse = {
+  uuid: '123',
+  verticalResults: []
+};
+
+/* A sample Vertical Search response. */
+const mockedVerticalResponse: VerticalSearchResponse = {
+  uuid: '123',
+  queryId: 'query-id',
+  verticalResults: null
+};
+
+/**
+ * Generates a mocked SearchService using the provided responses.
+ * 
+ * @param universalResponse - The response to supply for all Universal Searches. If one is
+ * not provided, a default will be used.
+ * @param verticalResponse - The response to supply for all Vertical Searches. If one is
+ * not provided, a default will be used.
+ */
+export function generateMockedSearchService(
+  universalResponse?: UniversalSearchResponse,
+  verticalResponse?: VerticalSearchResponse ): SearchService
+{
+  return {
+    universalSearch: (request: UniversalSearchRequest) => {
+      const response = universalResponse ? universalResponse : mockedUniversalResponse;
+      return Promise.resolve(response);
+    },
+    verticalSearch: (request: VerticalSearchRequest) => {
+      const response = verticalResponse ? verticalResponse : mockedVerticalResponse;
+      return Promise.resolve(response);
+    }
+  };
+}
+
+export const MockedSearchService: SearchService = generateMockedSearchService();
