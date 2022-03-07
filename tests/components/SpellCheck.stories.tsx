f1fa9dcd08b286794bda58bc6c9cd2407febfc02
Tom Meyer <tmeyer@yext.com>
2022-03-07 10:54:47 -0500

    Ignore ESLint rules in Story.

--- tests/components/SpellCheck.stories.tsx
+++ tests/components/SpellCheck.stories.tsx
@@ -6,11 +6,13 @@ import { SpellCheck } from '../../src/components/SpellCheck';
 
 import { MockedHeadless } from '../__fixtures__/answers-headless';
 
+// eslint-disable-next-line import/no-default-export
 export default {
   title: 'SpellCheck',
   component: SpellCheck,
 } as ComponentMeta<typeof SpellCheck>;
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const Primary = () => {
   return (
     <AnswersHeadlessContext.Provider value={MockedHeadless}>
