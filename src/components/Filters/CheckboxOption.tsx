63ede95ea933d084a9bb01a475207ea7601c551f
Tom Meyer <tmeyer@yext.com>
2022-03-15 16:16:22 -0400

    Allow a `Filter` option to be selected by default. (#64)
    This PR adds a `selectedByDefault` attribute to `CheckboxOptionsProps`. This attribute allows a particular option (filter) to be selected by default. This attribute was missed in the Compound Components refactor of Facets/Filters.
    
    J=SLAP-1932
    TEST=manual
    
    Tested the following:
    - When `selectedByDefault` is true, the Filter is checked and applied on page load.
    - De-selecting a Filter that is applied by default works as expected.
    - Selecting the Filter after de-selection works as expected.

--- src/components/Filters/CheckboxOption.tsx
+++ src/components/Filters/CheckboxOption.tsx
@@ -1,10 +1,10 @@
-import { Matcher, useAnswersUtilities } from '@yext/answers-headless-react';
-import { useCallback, useMemo } from 'react';
+import { Filter, Matcher, useAnswersUtilities } from '@yext/answers-headless-react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { v4 as uuid } from 'uuid';
 import { useFiltersContext } from './FiltersContext';
 import { useFilterGroupContext } from './FilterGroupContext';
 import { CompositionMethod, useComposedCssClasses } from '../../hooks/useComposedCssClasses';
-import { isDuplicateFilter } from '../../utils/filterutils';
+import { findSelectableFilter } from '../../utils/filterutils';
 
 /**
  * Props for the {@link Filters.CheckboxOption}
@@ -23,6 +23,8 @@ export interface CheckboxOptionProps {
    * an error is logged.
    */
   fieldId?: string,
+  /** If this particular filter should be selected by default. */
+  selectedByDefault?: boolean,
   /** The display label. Defaults to the value prop. */
   label?: string,
   /** CSS classes for customizing the component styling defined by {@link Filters.CheckboxCssClasses} */
@@ -60,6 +62,7 @@ export function CheckboxOption(props: CheckboxOptionProps): JSX.Element | null {
   const {
     fieldId = defaultFieldId,
     value,
+    selectedByDefault = false,
     label = props.value,
   } = props;
   const cssClasses = useComposedCssClasses(
@@ -83,32 +86,52 @@ export function CheckboxOption(props: CheckboxOptionProps): JSX.Element | null {
     handleClick(evt.target.checked);
   }, [handleClick]);
 
-  if (!fieldId) {
-    console.error('No fieldId found for filter with value', value);
-    return null;
-  }
-
-  if (typeof label !== 'string') {
-    console.error('A label is needed for filter with value', value);
-    return null;
-  }
-
-  if (!answersUtilities.isCloseMatch(label.toString(), searchValue)) {
-    return null;
-  }
-
-  const isSelected = !!filters?.find(storedDisplayableFilter => {
-    const { selected, displayName:_, ...storedFilter } = storedDisplayableFilter;
-    if (!selected) {
-      return false;
-    }
-    const targetFilter = {
-      fieldId,
+  const optionFilter: Filter = useMemo(() => {
+    return {
+      fieldId: fieldId ?? '',
       matcher: Matcher.Equals,
       value
     };
-    return isDuplicateFilter(storedFilter, targetFilter);
-  });
+  }, [fieldId, value]);
+  const existingStoredFilter = findSelectableFilter(optionFilter, filters);
+
+  const shouldRenderOption: boolean = useMemo(() => {
+    if (!fieldId) {
+      console.error('No fieldId found for filter with value', value);
+      return false;
+    }
+
+    if (typeof label !== 'string') {
+      console.error('A label is needed for filter with value', value);
+      return false;
+    }
+
+    if (!answersUtilities.isCloseMatch(label.toString(), searchValue)) {
+      return false;
+    }
+
+    return true;
+  }, [fieldId, value, answersUtilities, label, searchValue]);
+
+  useEffect(() => {
+    if (shouldRenderOption) {
+      if (!existingStoredFilter && selectedByDefault) {
+        selectFilter({
+          matcher: Matcher.Equals,
+          fieldId: optionFilter.fieldId,
+          value: optionFilter.value,
+          displayName: typeof label === 'string' ? label : undefined,
+          selected: true
+        });
+      }
+    }
+  }, [label, selectFilter, selectedByDefault, existingStoredFilter, optionFilter, shouldRenderOption]);
+
+  if (!shouldRenderOption) {
+    return null;
+  }
+
+  const isSelected = existingStoredFilter ? existingStoredFilter.selected : false;
 
   return (
     <div className={cssClasses.container}>
