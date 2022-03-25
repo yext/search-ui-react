eb7612c5e844d07f1eccd0bf6651886b591275b3
Yen Truong <36055303+yen-tt@users.noreply.github.com>
2022-03-25 12:25:25 -0400

    VerticalResults Storybook (#84)
    Add 4 stories for VerticalResults:
    - when there's no results
    - when there's results without pagination component
    - when there's results with pagination component
    - when it's in loading state
    
    J=SLAP-1993
    TEST=manual
    
    start up storybook and see the expected VerticalResults snapshots. The tailwind styling in storybook will be added in another item.
    <img width="305" alt="Screen Shot 2022-03-24 at 5 47 41 PM" src="https://user-images.githubusercontent.com/36055303/160015930-742aef75-9641-4272-b1a2-287bb84317d6.png">

---
+++ tests/components/VerticalResults.stories.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { AnswersHeadlessContext, Source } from '@yext/answers-headless-react';
+
+import { generateMockedHeadless } from '../__fixtures__/answers-headless';
+import { VerticalResults, VerticalResultsProps } from '../../src/components/VerticalResults';
+import { StandardCard } from '../../src/components/cards/StandardCard';
+
+const meta: ComponentMeta<typeof VerticalResults> = {
+  title: 'VerticalResults',
+  component: VerticalResults,
+};
+export default meta;
+
+const mockedHeadlessState = {
+  vertical: {
+    results: [
+      {
+        rawData: {
+          name: 'title1',
+          description: 'text1',
+          c_primaryCTA: {
+            link: 'link1',
+            label: 'job1',
+            linkType: 'link'
+          },
+          c_secondaryCTA: {
+            link: 'link2',
+            label: 'job2',
+            linkType: 'link'
+          }
+        },
+        source: Source.KnowledgeManager,
+        id: 'id1'
+      }
+    ],
+    resultsCount: 5,
+    limit: 1
+  }
+};
+
+export const NoResults = () => {
+  const verticalResultsProps: VerticalResultsProps = {
+    CardComponent: StandardCard
+  };
+  return (
+    <AnswersHeadlessContext.Provider value={generateMockedHeadless({})}>
+      <VerticalResults {...verticalResultsProps}/>
+    </AnswersHeadlessContext.Provider>
+  );
+};
+
+export const HasResultsWithoutPagination = () => {
+  const verticalResultsProps: VerticalResultsProps = {
+    CardComponent: StandardCard,
+    allowPagination: false
+  };
+  return (
+    <AnswersHeadlessContext.Provider value={generateMockedHeadless(mockedHeadlessState)}>
+      <VerticalResults {...verticalResultsProps}/>
+    </AnswersHeadlessContext.Provider>
+  );
+};
+
+export const HasResultsWithPagination = () => {
+  const verticalResultsProps: VerticalResultsProps = {
+    CardComponent: StandardCard,
+    allowPagination: true
+  };
+  return (
+    <AnswersHeadlessContext.Provider value={generateMockedHeadless(mockedHeadlessState)}>
+      <VerticalResults {...verticalResultsProps}/>
+    </AnswersHeadlessContext.Provider>
+  );
+};
+
+export const Loading = () => {
+  const verticalResultsProps: VerticalResultsProps = {
+    CardComponent: StandardCard
+  };
+  return (
+    <AnswersHeadlessContext.Provider value={generateMockedHeadless({
+      ...mockedHeadlessState,
+      searchStatus: { isLoading: true }
+    })}>
+      <VerticalResults {...verticalResultsProps}/>
+    </AnswersHeadlessContext.Provider>
+  );
+};
