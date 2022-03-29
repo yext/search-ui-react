cf1e730e9052ced70c417a1f2704fce60f0d2f90
Tom Meyer <tmeyer@yext.com>
2022-03-29 12:03:21 -0400

    Ensure Tailwind styles are used when generating Stories.
    Storybook now uses PostCSS to ensure the Tailwind styling is properly
    included in all stories. To do this, I had to modify the Storybook config,
    add a PostCSS config, and add Tailwind infra. I did not touch the existing
    Tailwind infra in the `test-site`. I think the top-level Tailwind setup
    and the test site's setup should be totally independent.
    
    J=SLAP-2008
    TEST=auto
    
    I did `npm run storybook` and saw the Tailwind styles applied. I also ensured
    the `build-storybook` command ran without issue.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,18 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: '@storybook/react'
 };
