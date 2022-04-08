945afe20d482d18f3f31a68c1730da40594ae689
Yen Truong <ytruong@yext.com>
2022-04-08 11:43:01 -0400

    doc update

--- tests/__fixtures__/core/AnswersCore.ts
+++ tests/__fixtures__/core/AnswersCore.ts
@@ -63,7 +63,7 @@ export class AnswersCore {
 
 
 /**
- * The decorator to be used in ./storybook/preview to read story-specific data off
+ * The decorator to be used in .storybook/preview to read story-specific data off
  * the story's parameters to mock AnswersCore's services on a per-story basis.
  */
 export function AnswersCoreDecorator(story, { parameters }) {
