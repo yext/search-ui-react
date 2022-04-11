71fbfc92f49c6da5ac742d9a45333750636688a7
nmanu1 <nmanu@yext.com>
2022-04-11 15:05:52 -0400

    Add Tailwind theme

--- .storybook/tailwind.config.js
+++ .storybook/tailwind.config.js
@@ -2,6 +2,21 @@ module.exports = {
   content: [
     './src/**/*.{ts,tsx}'
   ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': 'var(--primary-color, #2563eb)',
+        'primary-light': 'var(--primary-color-light, #dbeafe)',
+        'primary-dark':  'var(--primary-color-dark, #dbeafe)',
+        'neutral': 'var(--neutral-color, #4b5563)',
+        'neutral-light': 'var(--neutral-color-light, #9ca3af)',
+        'neutral-dark': 'var(--neutral-color-dark, #1f2937)'
+      },
+      borderRadius: {
+        cta: 'var(--cta-border-radius, 1rem)'
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/forms')({
       strategy: 'class',
