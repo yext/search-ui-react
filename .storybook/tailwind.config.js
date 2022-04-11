6a023e2ceb9bac0421f9f49613032ed60e765bfe
nmanu1 <nmanu@yext.com>
2022-04-11 17:28:28 -0400

    Remove unnecessary variables

--- .storybook/tailwind.config.js
+++ .storybook/tailwind.config.js
@@ -5,16 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': 'var(--primary-color, #2563eb)',
-        'primary-light': 'var(--primary-color-light, #dbeafe)',
-        'primary-dark':  'var(--primary-color-dark, #dbeafe)',
-        'neutral': 'var(--neutral-color, #4b5563)',
-        'neutral-light': 'var(--neutral-color-light, #9ca3af)',
-        'neutral-dark': 'var(--neutral-color-dark, #1f2937)'
-      },
-      borderRadius: {
-        cta: 'var(--cta-border-radius, 1rem)'
-      },
+        'primary': '#2563eb',
+        'primary-light': '#dbeafe',
+        'primary-dark':  '#dbeafe',
+        'neutral': '#4b5563',
+        'neutral-light': '#9ca3af',
+        'neutral-dark': '#1f2937'
+      }
     },
   },
   plugins: [
