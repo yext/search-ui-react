0825f6ae2673d559ae6ad4ce81822d93a8ca45f3
nmanu1 <88398086+nmanu1@users.noreply.github.com>
2022-04-12 11:41:38 -0400

    Add Storybook tests for UniversalResults (#106)
    Adds Storybook tests for the `UniversalResults` component. Add a Tailwind theme with the default primary and neutral colors to improve checking for WCAG violations.
    
    J=SLAP-2037
    TEST=manual
    
    Spin up Storybook locally and see the 2 new stories are added and display results as expected.

---
+++ tests/__fixtures__/data/universalresults.ts
@@ -0,0 +1,73 @@
+import { Matcher, Source, VerticalResults } from '@yext/answers-headless-react';
+
+const vertical1: VerticalResults = {
+  appliedQueryFilters: [{
+    displayKey: 'Job Category',
+    displayValue: 'Sales',
+    filter: {
+      fieldId: 'c_jobCategory',
+      matcher: Matcher.Equals,
+      value: 'Sales'
+    }
+  }],
+  queryDurationMillis: 500,
+  results: [{
+    rawData: {
+      name: 'title1',
+      description: 'text1',
+      c_primaryCTA: {
+        link: 'link1',
+        label: 'job1',
+        linkType: 'link'
+      },
+      c_secondaryCTA: {
+        link: 'link2',
+        label: 'job2',
+        linkType: 'link'
+      },
+    },
+    source: Source.KnowledgeManager,
+    id: 'id1'
+  }, {
+    rawData: {
+      name: 'title2',
+      description: 'text2'
+    },
+    source: Source.KnowledgeManager,
+    id: 'id2'
+  }],
+  resultsCount: 2,
+  source: Source.KnowledgeManager,
+  verticalKey: 'vertical1'
+};
+
+const vertical2: VerticalResults = {
+  appliedQueryFilters: [],
+  queryDurationMillis: 500,
+  results: [{
+    rawData: {
+      name: 'title3',
+      description: 'text3'
+    },
+    source: Source.KnowledgeManager,
+    id: 'id3'
+  }],
+  resultsCount: 1,
+  source: Source.KnowledgeManager,
+  verticalKey: 'vertical2'
+};
+
+const vertical3: VerticalResults = {
+  appliedQueryFilters: [],
+  queryDurationMillis: 500,
+  results: [],
+  resultsCount: 0,
+  source: Source.KnowledgeManager,
+  verticalKey: 'vertical3'
+};
+
+export const verticalResults = [
+  vertical1,
+  vertical2,
+  vertical3
+];
