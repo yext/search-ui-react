e6a8a0ceb04e6d8f27bc0a2b14ef2f6f89e36ac0
Connor Anderson <canderson@yext.com>
2022-04-20 13:58:03 -0400

    install-peers-proto

---
+++ install-npm.js
@@ -0,0 +1,43 @@
+
+// do it inline in sync way
+// to make it work in non-npm environment
+var npmBin
+  , executioner
+  , path = require('path')
+  , node = process.argv[0]
+  ;
+
+if (process.env['npm_execpath']) {
+  var execPath = process.env['npm_execpath'];
+  var expectedPath = path.join('bin', 'npm-cli.js');
+
+  if (execPath.slice(-1 * expectedPath.length) === expectedPath) {
+    npmBin = path.resolve(execPath);
+  }
+}
+
+// if no npm module found, don't expose any function
+// to allow upstream modules find alternatives
+module.exports = null;
+
+if (npmBin) {
+  executioner = require('executioner');
+
+  module.exports = function(packages, config, done) {
+    var options = {
+      node    : node,
+      npm     : npmBin,
+      // escape package name@versions
+      packages: packages.map((pkg) => '"' + pkg + '"').join(' ')
+    };
+
+    executioner('"${node}" "${npm}" install --no-save --no-package-lock ${packages}', options, function (error, result) {
+      if (error) {
+        console.error('Unable to install peerDependencies', error);
+        process.exit(1);
+        return;
+      }
+      done(result);
+    });
+  }
+}
---
+++ install-yarn.js
@@ -0,0 +1,45 @@
+// do it inline in sync way
+// to make it work in non-npm environment
+var yarnBin
+  , executioner
+  , path = require('path')
+  , node = process.argv[0]
+  ;
+
+if (process.env['npm_execpath'] && process.env['npm_execpath'].match(/bin[\/\\]yarn\.js$/)) {
+  var execPath = process.env['npm_execpath'];
+  var expectedPath = path.join('bin', 'yarn.js');
+
+  if (execPath.slice(-1 * expectedPath.length) === expectedPath) {
+    yarnBin = path.resolve(execPath, '..', '..', 'lib', 'cli');
+  }
+}
+
+// if no yarn module found, don't expose any function
+// to allow upstream modules find alternatives
+module.exports = null;
+
+if (yarnBin) {
+  executioner = require('executioner');
+
+  module.exports = function(packages, extra, done) {
+    var options = {
+      node    : node,
+      yarn    : yarnBin,
+      // escape package names@versions
+      packages: packages.map((pkg) => '"' + pkg + '"').join(' ')
+    };
+
+    executioner('"${node}" "${yarn}" add --no-lockfile ${packages}', options, function(error, result) {
+      if (error) {
+        console.error('Unable to install peerDependencies', error);
+        process.exit(1);
+        return;
+      }
+      done(result);
+    });
+
+    // Looks like yarn shows last line from the output of sub-scripts
+    console.log('Installing peerDependencies as devDependencies...');
+  };
+}
