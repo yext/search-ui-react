cec598d4878eba486497f87c4f79faabe41308a8
Yen Truong <ytruong@yext.com>
2022-05-10 14:25:21 -0400

    add 2 stories for location bias

--- .storybook/tailwind.config.cjs
+++ .storybook/tailwind.config.cjs
@@ -11,6 +11,16 @@ module.exports = {
         'neutral': '#4b5563',
         'neutral-light': '#9ca3af',
         'neutral-dark': '#1f2937'
+      },
+      keyframes: {
+        rotate: {
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        dash: {
+          '0%': { transform: 'rotate(0deg)', 'stroke-dashoffset': 204 },
+          '50%': { transform: 'rotate(45deg)', 'stroke-dashoffset': 52 },
+          '100%': { transform: 'rotate(360deg)', 'stroke-dashoffset': 204 },
+        }
       }
     },
   },
--- src/utils/search-operations.ts
+++ src/utils/search-operations.ts
@@ -5,36 +5,6 @@ import {
   SearchTypeEnum
 } from '@yext/answers-headless-react';
 
-const defaultGeolocationOptions: PositionOptions = {
-  enableHighAccuracy: false,
-  timeout: 6000,
-  maximumAge: 300000,
-};
-
-/**
- * If the provided search intents include a 'NEAR_ME' intent and there's no existing
- * user's location in state, retrieve and store user's location in headless state.
- *
- * @public
- */
-export async function updateLocationIfNeeded(
-  answersActions: AnswersActions,
-  intents: SearchIntent[],
-  geolocationOptions?: PositionOptions
-): Promise<void> {
-  if (intents.includes(SearchIntent.NearMe) && !answersActions.state.location.userLocation) {
-    try {
-      const position = await getUserLocation(geolocationOptions);
-      answersActions.setUserLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  }
-}
-
 /**
  * Executes a universal/vertical search.
  *
@@ -79,26 +49,4 @@ export async function getSearchIntents(
 ): Promise<SearchIntent[] | undefined> {
   const results = await executeAutocomplete(answersActions);
   return results?.inputIntents;
-}
-
-/**
- * Retrieves user's location using nagivator.geolocation API.
- *
- * @public
- */
-export async function getUserLocation(geolocationOptions?: PositionOptions): Promise<GeolocationPosition> {
-  return new Promise((resolve, reject) => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        position => resolve(position),
-        err => {
-          console.error('Error occured using geolocation API. Unable to determine user\'s location.');
-          reject(err);
-        },
-        { ...defaultGeolocationOptions, ...geolocationOptions }
-      );
-    } else {
-      reject('No access to geolocation API. Unable to determine user\'s location.');
-    }
-  });
 }
