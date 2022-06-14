40aee720c91a5a3f0b15c0a938f6bfc736646cd7
Alex Taing <59857107+alextaing@users.noreply.github.com>
2022-06-14 10:37:31 -0400

    useRef to return stable function references in useSynchronizedRequest (#178)
    This PR addresses `useSynchronizedRequest` used in the FilterSearch and SearchBar components.  Before, `useSynchronizedRequest` returned new `executeRequest` and `clearResponseData` functions on each call.  This change uses refs to return stable references to these functions to reduce re-rendering. 
    
    J=SLAP-2132
    TEST=manual, auto
    
    Auto testing uses react hooks testing library.
    Manual testing to check if reference to `executeRequest` function changed between calls (i.e. logging to console). It was also tested using `useMemo` that `useSynchronizedRequest` returned the same reference to `executeRequest` in FilterSearch.

---
+++ tests/hooks/useSynchronizedRequest.test.tsx
@@ -0,0 +1,52 @@
+import { useSynchronizedRequest } from '../../src/hooks/useSynchronizedRequest';
+import { renderHook } from '@testing-library/react-hooks';
+import { waitFor } from '@testing-library/react';
+
+it('returns an updated execute request function with the same reference', async () => {
+  let requestFunction = async () => 0;
+  const { result, rerender } = renderHook(() =>
+    useSynchronizedRequest(requestFunction)
+  );
+
+  const oldReturnedRequestFunction = result.current[1];
+  await waitFor(async () => {
+    expect(await oldReturnedRequestFunction()).toBe(0);
+  });
+
+  requestFunction = async () => 1;
+  rerender();
+
+  const newReturnedRequestFunction = result.current[1];
+  await waitFor(async () => {
+    expect(await newReturnedRequestFunction()).toBe(1);
+  });
+
+  expect(oldReturnedRequestFunction).toBe(newReturnedRequestFunction);
+});
+
+it('uses a new error function while returning same execute request reference', async () => {
+  const requestFunction = async () => {
+    throw new Error('ERROR');
+  };
+  let mockedErrorFunction = jest.fn().mockReturnValue(0);
+  const { result, rerender } = renderHook(() =>
+    useSynchronizedRequest(requestFunction, mockedErrorFunction)
+  );
+
+  const oldReturnedRequestFunction = result.current[1];
+  oldReturnedRequestFunction();
+  await waitFor(() => {
+    expect(mockedErrorFunction).toBeCalledTimes(1);
+  });
+
+  mockedErrorFunction = jest.fn().mockReturnValue(1);
+  rerender();
+
+  const newReturnedRequestFunction = result.current[1];
+  newReturnedRequestFunction();
+  await waitFor(() => {
+    expect(mockedErrorFunction).toBeCalledTimes(1);
+  });
+
+  expect(oldReturnedRequestFunction).toBe(newReturnedRequestFunction);
+});
