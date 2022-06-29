39de2d3cfafddca36c8d24d4e979f9a8fd17a707
Alex Taing <ataing@yext.com>
2022-06-29 09:58:16 -0400

    elseif for shft+tab and spaces around +/-

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -81,29 +81,30 @@ export function Dropdown(props: PropsWithChildren<DropdownProps>): JSX.Element {
     if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
       e.preventDefault();
     }
+
     if (!isActive) {
       return;
     }
+
     if (e.key === 'ArrowDown') {
       updateFocusedItem(focusedIndex + 1);
     } else if (e.key === 'ArrowUp') {
       updateFocusedItem(focusedIndex - 1);
     }
+
     if (e.key === 'Tab' && !e.shiftKey) {
       if (items.length !== 0) {
-        if (focusedIndex >= items.length-1) {
+        if (focusedIndex >= items.length - 1) {
           toggleDropdown(false);
           updateFocusedItem(-1);
         } else {
-          updateFocusedItem(focusedIndex+1);
+          updateFocusedItem(focusedIndex + 1);
           e.preventDefault();
         }
       }
-    }
-
-    if (e.key === 'Tab' && e.shiftKey) {
+    } else if (e.key === 'Tab' && e.shiftKey) {
       if (focusedIndex >= 0) {
-        updateFocusedItem(focusedIndex-1);
+        updateFocusedItem(focusedIndex - 1);
         e.preventDefault();
       } else {
         toggleDropdown(false);
