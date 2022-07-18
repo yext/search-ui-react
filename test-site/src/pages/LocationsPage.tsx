879dfa157d48abb42a5978ad48a41d1d820712eb
Alex Taing <ataing@yext.com>
2022-07-18 10:16:12 -0400

    add new page layout

--- test-site/src/pages/LocationsPage.tsx
+++ test-site/src/pages/LocationsPage.tsx
@@ -7,7 +7,8 @@ import {
   VerticalResults,
   LocationBias,
   GoogleMaps,
-  Pagination
+  Pagination,
+  GoogleMapsCssClasses
 } from '@yext/answers-react-components';
 import { useLayoutEffect } from 'react';
 
@@ -50,4 +51,51 @@ export function LocationsPage() {
       </div>
   );
 }
+
+// export function LocationsPage() {
+//   const answersActions = useAnswersActions();
+//   useLayoutEffect(() => {
+//     answersActions.setVertical('KM');
+//   });
+
+//   const customCssClasses: GoogleMapsCssClasses = {
+//     googleMapsContainer: 'h-screen'
+//   }
+
+//   return (
+//       <div>
+//         <SearchBar />
+//         <div className='flex'>
+//           <div className='min-w-fit pr-4'>
+//           </div>
+//           <div className='flex-grow'>
+//             <div className='flex items-baseline'>
+//               <ResultsCount />
+//               <AppliedFilters />
+//             </div>
+//             <div className='grid grid-cols-2 gap-4'>
+//               <div>
+//               <VerticalResults
+//                 CardComponent={StandardCard}
+//               />
+//               </div>
+//               <div>
+//                 <GoogleMaps 
+//                   apiKey='AIzaSyB5D45ghF1YMfqTLSzWubmlCN1euBVPhFw'
+//                   centerLatitude={37.540778} 
+//                   centerLongitude={-77.433928} 
+//                   defaultZoom={10} 
+//                   showEmptyMap={true}
+//                   providerOptions={providerOptions}
+//                   customCssClasses={customCssClasses}
+//                 />
+//               </div>
+//             </div>
+//             <Pagination />
+//             <LocationBias />
+//           </div>
+//         </div>
+//       </div>
+//   );
+// }
 
