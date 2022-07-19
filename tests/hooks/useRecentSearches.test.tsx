067b4d371879bcf009392aa5d647919a2bd6debf
Alex Taing <59857107+alextaing@users.noreply.github.com>
2022-07-19 16:01:46 -0400

    Separate recent searches in verticals (#242)
    This PR allows recent searches to show in verticals and keeps their recent search histories separate.
    
    J=SLAP-2241
    TEST=manual, auto
    
    The test site was used to check that recent search histories in different verticals (and in universal) were all kept separate. Jest tests were written for the `useRecentSearches` hook, testing that recent searches appear, that they stay on their own verticals, and that they can be cleared using the returned `clearRecentSearches` function.

---
+++ tests/hooks/useRecentSearches.test.tsx
@@ -0,0 +1,68 @@
+import { useRecentSearches } from '../../src/hooks/useRecentSearches';
+import { renderHook } from '@testing-library/react-hooks';
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+it('returns recent searches', () => {
+  const verticalKey = 'people';
+  const { result, rerender } = renderHook(() =>
+    useRecentSearches(10, verticalKey)
+  );
+  const setRecentSearch = result.current[1];
+  setRecentSearch('bob');
+  setRecentSearch('carrie');
+  setRecentSearch('williard');
+
+  rerender();
+  const [recentSearches] = result.current;
+  expect(recentSearches?.[0].query).toBe('williard');
+  expect(recentSearches?.[1].query).toBe('carrie');
+  expect(recentSearches?.[2].query).toBe('bob');
+});
+
+it('preserves recent searches when returning to a vertical', () => {
+  let verticalKey = 'people';
+  const { result, rerender } = renderHook(() =>
+    useRecentSearches(10, verticalKey)
+  );
+  const setRecentPeopleSearch = result.current[1];
+  setRecentPeopleSearch('bob');
+  setRecentPeopleSearch('carrie');
+  rerender();
+  let recentSearches = result.current[0];
+  expect(recentSearches?.length).toBe(2);
+
+  verticalKey = 'places';
+  rerender();
+  recentSearches = result.current[0];
+  const setRecentPlacesSearch = result.current[1];
+  expect(recentSearches?.length).toBe(0);
+
+  setRecentPlacesSearch('yext');
+  rerender();
+  recentSearches = result.current[0];
+  expect(recentSearches?.length).toBe(1);
+
+  verticalKey = 'people';
+  rerender();
+  recentSearches = result.current[0];
+  expect(recentSearches?.length).toBe(2);
+});
+
+it('clears searches properly', () => {
+  let verticalKey = 'people';
+  const { result, rerender } = renderHook(() =>
+    useRecentSearches(10, verticalKey)
+  );
+  const [, setRecentPeopleSearch, clearPeopleSearches] = result.current;
+  setRecentPeopleSearch('bob');
+  setRecentPeopleSearch('carrie');
+
+  clearPeopleSearches();
+  verticalKey = 'people';
+  rerender();
+  const recentSearches = result.current[0];
+  expect(recentSearches?.length).toBe(0);
+});
