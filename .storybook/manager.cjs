a68669ec8bcf6dbff9740b94c96616def1c89c74
Alex Taing <ataing@yext.com>
2022-07-27 15:52:32 -0400

    config settings

--- .storybook/manager.cjs
+++ .storybook/manager.cjs
@@ -3,3 +3,7 @@ const { addons } = require('@storybook/addons');
 addons.setConfig({
   theme: require('./yextTheme.cjs')
 });
+
+// ADD STORYBOOK ID
+// window.STORYBOOK_GA_ID = "UA-000000-01"
+// window.STORYBOOK_REACT_GA_OPTIONS = {}
