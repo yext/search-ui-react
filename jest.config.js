e3d0e3de316e79d9f43258422ea0357815d55f8f
Kwun Ying (Juliann) Zhou <kzhou@yext.com>
2022-07-27 12:17:33 -0400

    commit untracked files

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,17 @@ process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 require('react-scripts/config/env');
 
+
+const path = require('path');
+const createJestConfig = require('react-scripts/scripts/utils/createJestConfig');
+
+module.exports = createJestConfig(
+  relativePath => require.resolve(path.join('react-scripts', relativePath)),
+  __dirname, false
+);
 module.exports = {
+    verbose: true,
+
   "setupFilesAfterEnv": [
     "<rootDir>/tests/__setup__/setup-env.ts"
     ]
