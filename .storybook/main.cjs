c8c055804178037da88835e2fc821ee011c319bb
Alex Taing <ataing@yext.com>
2022-08-01 11:49:45 -0400

    set config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
+    '@storybook/addon-google-analytics',
     {
       name: '@storybook/addon-postcss',
       options: {
--- .storybook/manager.cjs
+++ .storybook/manager.cjs
@@ -3,3 +3,6 @@ const { addons } = require('@storybook/addons');
 addons.setConfig({
   theme: require('./yextTheme.cjs')
 });
+
+window.STORYBOOK_GA_ID = 'UA-24978808-17';
+window.STORYBOOK_REACT_GA_OPTIONS = {};
