b615ab2ebd95c63717ae3b0b10f61a31dc272359
Alex Taing <ataing@yext.com>
2022-08-01 15:20:21 -0400

    rename to cjs

--- .storybook/manager.cjs
+++ .storybook/manager.cjs
@@ -1,8 +1,8 @@
-window.STORYBOOK_GA_ID = 'UA-236173376-1';
-window.STORYBOOK_REACT_GA_OPTIONS = {};
-
 const { addons } = require('@storybook/addons');
 
 addons.setConfig({
   theme: require('./yextTheme.cjs')
 });
+
+window.STORYBOOK_GA_ID = 'UA-236173376-1';
+window.STORYBOOK_REACT_GA_OPTIONS = {};
