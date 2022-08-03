ce0303fde1175b453ff4e95c09d15dcfb6fa4fb7
Yen Truong <ytruong@yext.com>
2022-08-03 12:47:59 -0400

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
     {
       name: '@storybook/addon-postcss',
       options: {
