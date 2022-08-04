50438139669c4cbcc64ee49c9fc986d950497474
nmanu1 <nmanu@yext.com>
2022-08-04 15:06:08 -0400

    Reuse script

---
+++ tests/scripts/start-storybook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+start-storybook -p 6006 --quiet --ci &
+JOB_ID=$(echo $!) #get the background job ID
+
+# wait for a locally served Storybook
+attempt_counter=0
+max_attempts=30
+until $(curl --output /dev/null --silent --head http://localhost:6006)
+do
+  if [ ${attempt_counter} -eq ${max_attempts} ]
+  then
+    kill -9 $JOB_ID
+    echo "Max attempts reached"
+    exit 1
+  fi
+
+  echo 'waiting for a locally served Storybook in port 6006..'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 1
+done
