31f31fac8e60ee05c9a8667bfd77f2a72fa3cf6b
nmanu1 <nmanu@yext.com>
2022-08-04 17:05:14 -0400

    Abstract out wcag config

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,5 +1,6 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 import { Page } from 'playwright-core';
+import { runOnly } from './wcagConfig';
 
 /**
  * See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
@@ -12,10 +13,7 @@ const renderFunctions = {
   async postRender(page: Page, context) {
     await checkA11y(page, '#root', {
       axeOptions: {
-        runOnly: {
-          type: 'tag',
-          values: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']
-        },
+        runOnly,
         rules: {
           'color-contrast': { enabled: context.name !== 'Loading' }
         },
