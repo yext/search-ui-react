2fadcdd7d391fedcf5c9af0da19a8b05687d5a80
nmanu1 <nmanu@yext.com>
2022-08-04 16:49:56 -0400

    Fix spacing

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -2,9 +2,9 @@ import { injectAxe, checkA11y } from 'axe-playwright';
 import { Page } from 'playwright-core';
 
 /*
-* See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
-* to learn more about the test-runner hooks API.
-*/
+ * See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
+ * to learn more about the test-runner hooks API.
+ */
 const renderFunctions = {
   async preRender(page: Page) {
     await injectAxe(page);
