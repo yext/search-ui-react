692e508cc10f578e9a7945bc539597f76a6d1290
Yen Truong <ytruong@yext.com>
2022-08-03 22:32:51 -0400

    rename

--- tests/scripts/combined-coverage.sh
+++ tests/scripts/combined-coverage.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# run unit test and visual coverage test
-npm run test:unit
-npm run test:visual
+# Run unit test and visual coverage test by default.
+# Skip this step if merge-only flag is provided.
+echo $1
+if [ "$1" != "merge-only" ]
+then
+  echo 'running individual tests...'
+  npm run test:unit
+  npm run test:visual
+fi
 
 # merge
 mkdir -p coverage/merge
@@ -10,4 +16,4 @@ cp coverage/unit/coverage-final.json coverage/merge/coverage-unit.json
 cp coverage/visual/coverage-storybook.json coverage/merge/coverage-storybook.json
 
 nyc report --reporter=lcov --reporter=text -t coverage/merge --report-dir coverage/merge
-cp coverage/merge/lcov.info coverage/lcov.info 
+cp coverage/merge/lcov.info coverage/lcov.info
