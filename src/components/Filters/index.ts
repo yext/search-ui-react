f2fb3005b4d0ae08cd38c3001b2f4c8a21011d06
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-05 17:45:55 -0400

    publicly export

--- src/components/Filters/index.ts
+++ src/components/Filters/index.ts
@@ -2,6 +2,7 @@ export { CheckboxOption, CheckboxOptionProps, CheckboxCssClasses, FilterOptionCo
 export { CollapsibleLabel, CollapsibleLabelProps } from './CollapsibleLabel';
 export { CollapsibleSection, CollapsibleSectionProps } from './CollapsibleSection';
 export { FacetsProvider, FacetsProviderProps } from './FacetsProvider';
+export { FilterDivider } from './FilterDivider';
 export { FiltersContext, useFiltersContext, FiltersContextType } from './FiltersContext';
 export { FilterGroupProvider, FilterGroupProviderProps } from './FilterGroupProvider';
 export { FilterGroupContext, useFilterGroupContext, FilterGroupContextType } from './FilterGroupContext';
