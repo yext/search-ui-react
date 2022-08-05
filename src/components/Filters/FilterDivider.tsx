790692d61b034fea5cebb250089027e59de1a2af
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-05 17:34:18 -0400

    fix styling issue

--- src/components/Filters/FilterDivider.tsx
+++ src/components/Filters/FilterDivider.tsx
@@ -1,3 +1,3 @@
 export function FilterDivider({ className ='w-full h-px bg-gray-200 my-4' }: { className?: string }): JSX.Element {
-    return <div className={className} />;
+  return <div className={className} />;
 }
