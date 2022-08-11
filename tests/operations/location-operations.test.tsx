e3dcd78a53e0f404eb29e45f6ae8e8670dd2542e
Cindy Yang <cyang@yext.com>
2022-08-11 10:45:25 -0400

    added test for set location

--- tests/operations/location-operations.test.tsx
+++ tests/operations/location-operations.test.tsx
@@ -1,23 +1,36 @@
-import { SearchHeadless, SearchIntent } from '@yext/search-headless-react';
+import { SearchIntent } from '@yext/search-headless-react';
 import { generateMockedHeadless } from '../__fixtures__/search-headless';
 import { updateLocationIfNeeded } from '../__fixtures__/utils/location-operations';
 
 describe('Location Operations updateLocationIfNeeded', () => {
-  let searchActions: SearchHeadless;
-  beforeEach(() => {
-    searchActions = generateMockedHeadless();
-  });
 
-  it('fires getUserLocation with NearMe intent', async () => {
+  it('fires setUserLocation with NearMe intent', async () => {
+    const searchActions = generateMockedHeadless();
     searchActions.setUserLocation = jest.fn();
     const intents: SearchIntent[] = [SearchIntent.NearMe];
     await updateLocationIfNeeded(searchActions, intents);
     expect(searchActions.setUserLocation).toHaveBeenCalled();
   });
 
-  it('does not fire getUserLocation without NearMe intent', async () => {
+  it('does not fire setUserLocation without NearMe intent', async () => {
+    const searchActions = generateMockedHeadless();
     searchActions.setUserLocation = jest.fn();
     const intents: SearchIntent[] = [];
+    await updateLocationIfNeeded(searchActions, intents);
+    expect(searchActions.setUserLocation).not.toHaveBeenCalled();
+  });
+
+  it('does not fire setUserLocation if already set', async () => {
+    const searchActions = generateMockedHeadless({
+      location: {
+        userLocation: {
+          latitude: 38.9072,
+          longitude: -77.0369
+        }
+      }
+    });
+    searchActions.setUserLocation = jest.fn();
+    const intents: SearchIntent[] = [SearchIntent.NearMe];
     await updateLocationIfNeeded(searchActions, intents);
     expect(searchActions.setUserLocation).not.toHaveBeenCalled();
   });
