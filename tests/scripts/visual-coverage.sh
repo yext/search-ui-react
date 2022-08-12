25633d491191fc7f9f11f46a9a569387315bbbc7
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-12 13:25:06 -0400

    update visual test script to kill process (#291)
    This PR updates visual-coverage and start-storybook test script to only kill any process started by the test at the end of the test. If there are pre-existing process on port, the script assumes the process is a storybook instance and use it for test-runner coverage instead of starting a new storybook, and leaves the pre-existing process running after the test.
    
    J=SLAP-2323
    TEST=manual
    
    Test: tested by running npm run test:visual without preexisting process on port, the test runs as expected and the storybook process is killed at the end of the test; also ran the visual test with a preexisting storybook process on port, the test-runner uses the existing storybook for coverage report and leaves the storybook process running after the test as expected.

--- tests/scripts/visual-coverage.sh
+++ tests/scripts/visual-coverage.sh
@@ -6,9 +6,11 @@
 # generate coverage from storybook test-runner
 test-storybook --coverage
 
-# kill the locally served Storybook
-kill -9 $JOB_ID
-
+# kill the locally served Storybook started by the test
+if [[ -z $PREEXISTING_PORT_PROCESS ]]
+then
+  pkill -P $NEW_STORYBOOK_JOB_ID
+fi
 # generate lcov coverage for visual tests from story book
 nyc report --reporter=lcov -t coverage/storybook --report-dir coverage/visual
 
