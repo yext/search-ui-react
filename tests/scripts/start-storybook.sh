4496cace9602d5296b0294b1d35238c16410e23c
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-12 10:11:06 -0400

    kill all child processes created by storybook and only kill storybook started by the test

--- tests/scripts/start-storybook.sh
+++ tests/scripts/start-storybook.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-npm run storybook -- --quiet --ci &
+#check for existing process on port:6006
+process=`lsof -i :6006`
+if [[ -z $process ]]
+then
+  echo "port:6006 available - starting storybook"
+  npm run storybook -- --quiet --ci &
+  JOB_ID=$(echo $!)
 
-# wait for a locally served Storybook
-attempt_counter=0
-max_attempts=30
-until $(curl --output /dev/null --silent --head http://localhost:6006)
-do
-  if [ ${attempt_counter} -eq ${max_attempts} ]
-  then
-    npx kill-port 6006
-    echo "Max attempts reached"
-    exit 1
-  fi
-
-  echo "waiting for a locally served Storybook in port 6006.."
-  attempt_counter=$(($attempt_counter+1))
-  sleep 1
-done
+  # wait for a locally served Storybook
+  attempt_counter=0
+  max_attempts=30
+  until $(curl --output /dev/null --silent --head http://localhost:6006)
+  do
+    if [ ${attempt_counter} -eq ${max_attempts} ]
+    then
+      pkill -P $JOB_ID
+      echo "Max attempts reached"
+      exit 1
+    fi
+    echo "waiting for a locally served Storybook in port 6006.."
+    attempt_counter=$(($attempt_counter+1))
+    sleep 1
+  done
+else
+  echo -e "port:6006 in use - storybook already started: \n $process"
+fi
--- tests/scripts/visual-coverage.sh
+++ tests/scripts/visual-coverage.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-#check for existing process on port:6006
-process=`lsof -i :6006`
+# start Storybook in the same shell
+. ./tests/scripts/start-storybook.sh
+
+# generate coverage from storybook test-runner
+test-storybook --coverage
+
+# kill the locally served Storybook started by the test
 if [[ -z $process ]]
 then
-  echo "port:6006 available - starting storybook"
-  # start Storybook in the same shell
-  . ./tests/scripts/start-storybook.sh
-
-  # generate coverage from storybook test-runner
-  test-storybook --coverage
-
-  # kill the locally served Storybook
-  npx kill-port 6006
-
- # generate lcov coverage for visual tests from story book
-  nyc report --reporter=lcov -t coverage/storybook --report-dir coverage/visual
-
-  cp coverage/storybook/coverage-storybook.json coverage/visual/coverage-storybook.json
-  rm -r coverage/storybook
-else
-  echo -e "port:6006 in use - please kill current process before starting storybook: \n $process"
+  pkill -P $JOB_ID
 fi
+# generate lcov coverage for visual tests from story book
+nyc report --reporter=lcov -t coverage/storybook --report-dir coverage/visual
+
+cp coverage/storybook/coverage-storybook.json coverage/visual/coverage-storybook.json
+rm -r coverage/storybook
