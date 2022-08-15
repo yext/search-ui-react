7f77a89f422140d377656181cb741920ec5b58db
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-15 17:15:35 -0400

    unset NODE_OPTIONS

--- tests/scripts/visual-coverage.sh
+++ tests/scripts/visual-coverage.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# set NODE_OPTIONS for node 17/18
+if [[ $CURRENT_NODE_VERSION =~ 17(.*)|18(.*) ]]
+then
+  echo -e "currently using node version 17+ - setting NODE_OPTIONS \n"
+  export NODE_OPTIONS=--openssl-legacy-provider
+fi
+
 # start Storybook in the same shell
 . ./tests/scripts/start-storybook.sh
 
@@ -16,3 +23,6 @@ nyc report --reporter=lcov -t coverage/storybook --report-dir coverage/visual
 
 cp coverage/storybook/coverage-storybook.json coverage/visual/coverage-storybook.json
 rm -r coverage/storybook
+
+# unset NODE_OPTIONS
+unset NODE_OPTIONS
