44c580e5a420473b899d41b173c3a0d5a1e546b5
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-16 10:07:05 -0400

    delete line and move script to start-storybook

--- tests/scripts/visual-coverage.sh
+++ tests/scripts/visual-coverage.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# set NODE_OPTIONS for node 17/18
-if [[ $CURRENT_NODE_VERSION =~ 17(.*)|18(.*) ]]
-then
-  echo -e "currently using node version 17+ - setting NODE_OPTIONS \n"
-  export NODE_OPTIONS=--openssl-legacy-provider
-fi
-
 # start Storybook in the same shell
 . ./tests/scripts/start-storybook.sh
 
@@ -23,6 +16,3 @@ nyc report --reporter=lcov -t coverage/storybook --report-dir coverage/visual
 
 cp coverage/storybook/coverage-storybook.json coverage/visual/coverage-storybook.json
 rm -r coverage/storybook
-
-# unset NODE_OPTIONS
-unset NODE_OPTIONS
