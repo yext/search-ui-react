acb6591d3651cc7a330e9b63d6187a574db63bf3
Oliver Shi <oshi@yext.com>
2022-10-27 14:36:41 -0400

    try with one line

--- src/components/utils/renderToContainer.tsx
+++ src/components/utils/renderToContainer.tsx
@@ -4,9 +4,7 @@ type Renderer = (reactElement: ReactElement, container: Element) => void;
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore we support both react-dom 17 and 18, react-dom/client does not exist in react-dom 17
-const reactDomClientPromise = import('react-dom/client');
-
-const rendererPromiseWithFallback: Promise<Renderer> = reactDomClientPromise.then(reactDomClient => {
+const rendererPromiseWithFallback: Promise<Renderer> = import('react-dom/client').then(reactDomClient => {
   const { createRoot } = reactDomClient;
   const render: Renderer = (reactElement, container) => {
     const root = createRoot(container);
