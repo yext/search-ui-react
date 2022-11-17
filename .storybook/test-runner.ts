06c772596c36d4d08836b12f3b6e4998ac72fb03
Yen Truong <36055303+yen-tt@users.noreply.github.com>
2022-11-17 09:38:52 -0500

    Fix wcag github action mapbox issues (#337)
    paired with the changes in this [PR](https://github.com/yext/slapshot-reusable-workflows/pull/22) in WCAG workflow, this PR updates the WCAG github action in the repo to pass in the mapbox key. Also updated the wcag test to exclude checking elements coming from mapbox canvas container as any potential violations coming from there is outside of our repo's control.
    
    WCAG github action also run on pull request to feature branch now.
    
    J=SLAP-2458
    TEST=auto
    
    see that WCAG github action now passes

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,28 +1,36 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 import { Page } from 'playwright-core';
 import { runOnly } from './wcagConfig';
+import { TestContext, TestRunnerConfig } from '@storybook/test-runner';
 
 /**
  * See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
  * to learn more about the test-runner hooks API.
  */
-const renderFunctions = {
+const renderFunctions: TestRunnerConfig = {
   async preRender(page: Page) {
     await injectAxe(page);
   },
-  async postRender(page: Page, context) {
-    await checkA11y(page, '#root', {
-      axeOptions: {
-        runOnly,
-        rules: {
-          'color-contrast': { enabled: context.name !== 'Loading' }
+  async postRender(page: Page, context: TestContext) {
+    await checkA11y(
+      page,
+      {
+        include: ['#root'],
+        exclude: ['#root .mapboxgl-canvas-container'],
+      },
+      {
+        axeOptions: {
+          runOnly,
+          rules: {
+            'color-contrast': { enabled: context.name !== 'Loading' },
+          },
         },
-      },
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true,
-      },
-    });
+        detailedReport: true,
+        detailedReportOptions: {
+          html: true,
+        },
+      }
+    );
   },
 };
 
