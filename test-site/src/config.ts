65622fed786c0611176a1a386b0c0ce808d3db6f
Connor Anderson <canderson@yext.com>
2023-05-10 10:16:32 -0400

    Prepare v1.2.0

--- test-site/src/config.ts
+++ test-site/src/config.ts
@@ -1,7 +1,11 @@
+import { CloudRegion , Environment} from '@yext/search-headless-react';
+
 export const config = {
   apiKey: '2d8c550071a64ea23e263118a2b0680b',
   experienceKey: 'slanswers-hier-facets',
   locale: 'en',
   experienceVersion: 'STAGING',
-  businessId: 123123
+  businessId: 123123,
+  cloudRegion: CloudRegion.US,
+  environment: Environment.PROD,
 };
