2b50399f60a3d9c960d8d3c449464e86b10af991
Duval Kilpatrick <mkilpatrick@yext.com>
2023-08-08 18:26:22 -0400

    run storybook auto migration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-  extends: [
-    '@yext/slapshot/typescript-react'
-  ],
+  extends: ['@yext/slapshot/typescript-react', 'plugin:storybook/recommended'],
   ignorePatterns: ['lib', 'tests/setup/responses', 'storybook-static', '!.storybook'],
-  overrides: [
-    {
-      files: ['**/*.{test,stories}.*'],
-      rules: {
-        'react-perf/jsx-no-new-array-as-prop': 'off',
-        'react-perf/jsx-no-new-function-as-prop': 'off',
-        'react-perf/jsx-no-new-object-as-prop': 'off'
-      }
+  overrides: [{
+    files: ['**/*.{test,stories}.*'],
+    rules: {
+      'react-perf/jsx-no-new-array-as-prop': 'off',
+      'react-perf/jsx-no-new-function-as-prop': 'off',
+      'react-perf/jsx-no-new-object-as-prop': 'off'
     }
-  ]
+  }]
 };
