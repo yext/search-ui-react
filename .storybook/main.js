d4a269f42a83f83c1e0c689181d8afe6d3bc9501
Nidhi Manu <nmanu@yext.com>
2023-11-27 16:57:25 -0500

    Fix Storybook preview

--- .storybook/main.js
+++ .storybook/main.js
@@ -29,10 +29,6 @@ module.exports = {
   staticDirs: ['./public'],
 
   webpackFinal: async (config) => {
-    //use commonjs entry point for "@reach" packages
-    config.resolve.alias['@reach/auto-id'] = require.resolve('@reach/auto-id');
-    config.resolve.alias['@reach/utils'] = require.resolve('@reach/utils');
-
     config.resolve.alias['./SearchCore'] = require.resolve('../tests/__fixtures__/core/SearchCore.ts');
     config.resolve.alias['../utils/location-operations'] = require.resolve('../tests/__fixtures__/utils/location-operations.ts');
     return config;
