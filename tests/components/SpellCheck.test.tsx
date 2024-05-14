12a703df005bb114251516149fd589d33918c66f
Jesuye David <jdavid@yext.com>
2024-05-14 16:40:10 -0400

    update test to reduce coverage for find and comment action test

--- tests/components/SpellCheck.test.tsx
+++ tests/components/SpellCheck.test.tsx
@@ -36,42 +36,42 @@ describe('SpellCheck', () => {
     });
   });
 
-  it('Suggestion is formatted properly', () => {
-    render(<SpellCheck />);
-    expect(screen.getByText('Did you mean')).toBeDefined();
-    const correctedQuery = mockedState.spellCheck?.correctedQuery;
-    expect(correctedQuery && screen.getByText(correctedQuery)).toBeTruthy();
-  });
+  // it('Suggestion is formatted properly', () => {
+  //   render(<SpellCheck />);
+  //   expect(screen.getByText('Did you mean')).toBeDefined();
+  //   const correctedQuery = mockedState.spellCheck?.correctedQuery;
+  //   expect(correctedQuery && screen.getByText(correctedQuery)).toBeTruthy();
+  // });
 
-  it('Button\'s label is correct', () => {
-    render(<SpellCheck />);
-    const label = screen.getByRole('button').textContent;
-    expect(label).toEqual(mockedState.spellCheck?.correctedQuery);
-  });
+  // it('Button\'s label is correct', () => {
+  //   render(<SpellCheck />);
+  //   const label = screen.getByRole('button').textContent;
+  //   expect(label).toEqual(mockedState.spellCheck?.correctedQuery);
+  // });
 
-  it('Fires onClick when provided', async () => {
-    const props = {
-      onClick: jest.fn()
-    };
-    const onClick = jest.spyOn(props, 'onClick');
-    const actions = spyOnActions();
+  // it('Fires onClick when provided', async () => {
+  //   const props = {
+  //     onClick: jest.fn()
+  //   };
+  //   const onClick = jest.spyOn(props, 'onClick');
+  //   const actions = spyOnActions();
 
-    render(<SpellCheck {...props} />);
-    await userEvent.click(screen.getByRole('button'));
+  //   render(<SpellCheck {...props} />);
+  //   await userEvent.click(screen.getByRole('button'));
 
-    const verticalKey = mockedState.vertical?.verticalKey;
-    const correctedQuery = mockedState.spellCheck?.correctedQuery;
-    expect(actions.setQuery).toHaveBeenCalledWith(correctedQuery);
-    expect(onClick).toHaveBeenCalledWith({ correctedQuery, verticalKey });
-  });
+  //   const verticalKey = mockedState.vertical?.verticalKey;
+  //   const correctedQuery = mockedState.spellCheck?.correctedQuery;
+  //   expect(actions.setQuery).toHaveBeenCalledWith(correctedQuery);
+  //   expect(onClick).toHaveBeenCalledWith({ correctedQuery, verticalKey });
+  // });
 
-  it('Fires executeSearch when no onClick is provided', async () => {
-    const actions = spyOnActions();
-    render(<SpellCheck />);
-    await userEvent.click(screen.getByRole('button'));
+  // it('Fires executeSearch when no onClick is provided', async () => {
+  //   const actions = spyOnActions();
+  //   render(<SpellCheck />);
+  //   await userEvent.click(screen.getByRole('button'));
 
-    const correctedQuery = mockedState.spellCheck?.correctedQuery;
-    expect(actions.setQuery).toHaveBeenCalledWith(correctedQuery);
-    expect(actions.executeVerticalQuery).toHaveBeenCalledTimes(1);
-  });
+  //   const correctedQuery = mockedState.spellCheck?.correctedQuery;
+  //   expect(actions.setQuery).toHaveBeenCalledWith(correctedQuery);
+  //   expect(actions.executeVerticalQuery).toHaveBeenCalledTimes(1);
+  // });
 });
