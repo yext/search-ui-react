e3548a91a586f7bf373cf3023eea2a391f226ab4
Anh Thu Nguyen <anguyen@yext.com>
2024-06-13 18:12:42 -0400

    revert test-site config

--- test-site/src/config.ts
+++ test-site/src/config.ts
@@ -1,11 +1,11 @@
 import { CloudRegion, Environment } from '@yext/search-headless-react';
 
 export const config = {
-  apiKey: process.env.REACT_APP_LIVE_API_KEY || '01db1d1e5ebbaa7ea2e6807ad2196ab3',
-  experienceKey: 'hh-vector-testing',
+  apiKey: process.env.REACT_APP_LIVE_API_KEY || 'REPLACE_ME',
+  experienceKey: 'slanswers-hier-facets',
   locale: 'en',
   experienceVersion: 'STAGING',
-  businessId: 919871,
+  businessId: 123123,
   cloudRegion: CloudRegion.US,
   environment: Environment.PROD,
 };
