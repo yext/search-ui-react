1256b4489fde46c6214c837da2ca547b067fa590
Anh Thu Nguyen <anguyen@yext.com>
2024-09-13 18:59:56 -0400

    do not display GDA citations without name field
    J=CLIP-1565
    TEST=auto,manual
    
    added and ran jest test
    verfied locally using test-site that divider and citations section are not displayed without minimum citation data

--- tests/components/GenerativeDirectAnswer.test.tsx
+++ tests/components/GenerativeDirectAnswer.test.tsx
@@ -34,29 +34,18 @@ const CustomCitationsComponent = (props: CitationsProps) => {
 
 jest.mock('@yext/search-headless-react');
 
-describe('GenerativeDirectAnswer', () => {
+describe('GenerativeDirectAnswer with sufficient citation fields', () => {
   beforeEach(() => {
     mockAnswersState(mockedState);
   });
-
-  it('Answer text and citations are displayed', () => {
+  it('answer text and all citations are displayed', () => {
     render(<GenerativeDirectAnswer />);
-    const verticals = mockedState.universal?.verticals ?? [];
     expect(screen.getByText(generativeDirectAnswerResponse.directAnswer)).toBeDefined();
+    expect(screen.getByText('Sources (2)')).toBeDefined();
 
-    function checkResultData(resultData: Record<string, unknown>, isCitation: boolean) {
-      if (isCitation) {
-        expect(typeof resultData.name === 'string' && screen.getByText(resultData.name)).toBeTruthy();
-        expect(typeof resultData.description === 'string' && screen.getByText(resultData.description)).toBeTruthy();
-      } else {
-        expect(typeof resultData.name === 'string' && screen.queryByText(resultData.name)).toBeNull();
-        expect(typeof resultData.description === 'string' && screen.queryByText(resultData.description)).toBeNull();
-      }
-    }
-
-    checkResultData(verticals[0].results[0].rawData, false);
-    checkResultData(verticals[0].results[1].rawData, true);
-    checkResultData(verticals[1].results[0].rawData, true);
+    checkResultData(verticalResults[0].results[0].rawData, false); //not a citation
+    checkResultData(verticalResults[0].results[1].rawData, true);
+    checkResultData(verticalResults[1].results[0].rawData, true);
   });
 
   it('Citations component overridden with dummy text', () => {
@@ -67,3 +56,48 @@ describe('GenerativeDirectAnswer', () => {
     expect(screen.getByText("CustomCitationsComponentTest")).toBeTruthy();
   });
 });
+
+describe('GenerativeDirectAnswer without sufficient citation fields', () => {
+  it('first citation does not have name field but second citation does', () => {
+    verticalResults[0].results[1].rawData.name = undefined;
+    mockAnswersState({...mockedState, universal: {verticals: verticalResults}});
+
+    render(<GenerativeDirectAnswer />);
+    expect(screen.getByText(generativeDirectAnswerResponse.directAnswer)).toBeDefined();
+    expect(screen.getByText('Sources (1)')).toBeDefined();
+
+    checkResultData(verticalResults[0].results[0].rawData, false); //not a citation
+    checkResultData(verticalResults[0].results[1].rawData, false); //citation without name
+    checkResultData(verticalResults[1].results[0].rawData, true);
+  });
+
+  it('all citations do not have name field', () => {
+    verticalResults[1].results[0].rawData.name = undefined;
+    mockAnswersState({...mockedState, universal: {verticals: verticalResults}});
+
+    render(<GenerativeDirectAnswer />);
+    expect(screen.getByText(generativeDirectAnswerResponse.directAnswer)).toBeDefined();
+    expect(screen.queryByText('Sources')).toBeNull();
+
+    checkResultData(verticalResults[0].results[0].rawData, false); //not a citation
+    checkResultData(verticalResults[0].results[1].rawData, false); //citation without name
+    checkResultData(verticalResults[1].results[0].rawData, false); //citation without name
+  });
+});
+
+function checkResultData(resultData: Record<string, unknown>, shouldDisplay: boolean) {
+  if (typeof resultData.name === 'string') {
+    if (shouldDisplay) {
+      expect(screen.getByText(resultData.name)).toBeTruthy();
+    } else {
+      expect(screen.queryByText(resultData.name)).toBeNull();
+    }
+  }
+  if (typeof resultData.description === 'string') {
+    if (shouldDisplay) {
+      expect(screen.getByText(resultData.description)).toBeTruthy();
+    } else {
+      expect(screen.queryByText(resultData.description)).toBeNull();
+    }
+  } 
+}
