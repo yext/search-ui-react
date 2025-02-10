ee7c23c9ca7a8c72906113d5ac27bb790539053d
Chris Snyder <chris@chrissnyder.org>
2025-02-10 16:59:03 -0500

    Mark all icons as hidden for screen readers. (#485)
    * Mark StarIcon as Hidden to screenreaders
    
    * Mark all decortive icons as hidden for screen readers.

--- src/icons/VerticalDividerIcon.tsx
+++ src/icons/VerticalDividerIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function VerticalDividerIcon({ className }: { className?: string }): JSX.Element {
   return (
     <svg className={className}
-      width="1" height="24" viewBox="0 0 1 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      width="1" height="24" viewBox="0 0 1 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
       <rect width="1" height="24" rx="0.5" fill="#E1E5E8" />
     </svg>
   );
