84228a1beb2c8ccad80266a7155a0a8ac47b4fe7
anguyen-yext2 <anguyen@yext.com>
2025-05-28 15:42:11 -0400

    Add helper function to handle locales with underscores. Verified map is localized when locale is set to valid ones like 'en', 'en_US', 'ja', 'ja_JP', 'fr_FR', 'zh_Hans_CN', etc Verified map is in English when locale is invalid.

--- src/components/MapboxMap.tsx
+++ src/components/MapboxMap.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
 import mapboxgl, { MarkerOptions } from 'mapbox-gl';
-import Language from '@mapbox/mapbox-gl-language';
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
 import { Result, useSearchState } from '@yext/search-headless-react';
 import { useDebouncedFunction } from '../hooks/useDebouncedFunction';
 import ReactDOM from 'react-dom';
@@ -154,7 +154,7 @@ export function MapboxMap<T>({
   const map = useRef<mapboxgl.Map | null>(null);
   const markers = useRef<mapboxgl.Marker[]>([]);
 
-  const locale = useSearchState(state => state.meta?.locale);
+  const locale = useSearchState(state => state.meta.locale);
   const locationResults = useSearchState(state => state.vertical.results) as Result<T>[];
   const onDragDebounced = useDebouncedFunction(onDrag, 100);
   const [selectedResult, setSelectedResult] = useState<Result<T> | undefined>(undefined);
@@ -189,8 +189,8 @@ export function MapboxMap<T>({
           visualizePitch: false
         });
         mapbox.addControl(nav, 'top-right');
-        mapbox.addControl(new Language({
-          defaultLanguage: locale
+        mapbox.addControl(new MapboxLanguage({
+          defaultLanguage: getDefaultMapboxLanguage(locale)
         }));
         if (onDragDebounced) {
           mapbox.on('drag', () => {
@@ -300,4 +300,16 @@ function getDefaultCoordinate<T>(result: Result<T>): Coordinate | undefined {
     return undefined;
   }
   return yextDisplayCoordinate;
+}
+
+function getDefaultMapboxLanguage(locale?: string) {
+  if (locale) {
+    try {
+      const localeOptions = new Intl.Locale(locale.replaceAll('_', '-'));
+      return localeOptions.script ? `${localeOptions.language}-${localeOptions.script}` : localeOptions.language;
+    } catch (e) {
+      console.warn(`Locale "${locale}" is not supported.`)
+    }
+  }
+  return 'en';
 }
