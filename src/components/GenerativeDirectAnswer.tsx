7c5e04003af2aafe16a651bb066f4636e8db505b
Maxfield Kouzel <mkouzel@yext.com>
2025-08-04 14:50:58 -0400

    Revert "Debounce generative direct answer calls"
    This reverts commit 5523946e18836d10abc556e81115603600c9e8b7.

--- src/components/GenerativeDirectAnswer.tsx
+++ src/components/GenerativeDirectAnswer.tsx
@@ -11,7 +11,7 @@ import { useCardAnalytics } from '../hooks/useCardAnalytics';
 import { DefaultRawDataType } from '../models/index';
 import { executeGenerativeDirectAnswer } from '../utils/search-operations';
 import { Markdown, MarkdownCssClasses } from './Markdown';
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 
 /**
  * The CSS class interface used for {@link GenerativeDirectAnswer}.
@@ -83,6 +83,7 @@ export function GenerativeDirectAnswer({
   const universalResults = useSearchState(state => state.universal);
   const verticalResults = useSearchState(state => state.vertical);
   const searchId = useSearchState(state => state.meta.uuid);
+
   const searchResults: Result[] | undefined = React.useMemo(() => {
     if (isUniversal) {
       return universalResults.verticals?.flatMap(v => v.results);
@@ -91,19 +92,17 @@ export function GenerativeDirectAnswer({
     }
   }, [isUniversal, universalResults, verticalResults]);
 
-  const [lastExecutedSearchResults, setLastExecutedSearchResults] = useState(searchResults);
   const searchActions = useSearchActions();
   const gdaResponse = useSearchState(state => state.generativeDirectAnswer?.response);
   const isLoading = useSearchState(state => state.generativeDirectAnswer?.isLoading);
   const handleClickEvent = useReportClickEvent();
 
   React.useEffect(() => {
-    if (!searchResults?.length || !searchId || searchResults === lastExecutedSearchResults) {
+    if (!searchResults?.length || !searchId) {
       return;
     }
     executeGenerativeDirectAnswer(searchActions);
-    setLastExecutedSearchResults(searchResults);
-  }, [searchResults, searchId, lastExecutedSearchResults]);
+  }, [searchResults, searchId]);
 
   if (!searchResults?.length || isLoading || !gdaResponse || gdaResponse.resultStatus !== 'SUCCESS') {
     return null;
@@ -153,7 +152,7 @@ function Answer(props: AnswerProps) {
     }),
     [cssClasses.answerText]
   );
-
+  
   return <>
     <div className={cssClasses.header}>
       {answerHeader ?? t('aiGeneratedAnswer')}
